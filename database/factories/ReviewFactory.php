e77bffbfcdb63203696d415d0a3d383e5f4bf0f2
pradip <pradipraj8866@gmail.com>
2019-09-24 21:43:52 +0530

    Created Model -a

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\model\review;
+use Faker\Generator as Faker;
+
+$factory->define(review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
